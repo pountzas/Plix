1ba64cc333eef5d114da545117687d76c80ff151
pountzas <nikos@pountzas.gr>
2022-02-20 22:00:34 +0200

    import header to home page & fix styles

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,15 @@
 import Head from 'next/head';
-import Image from 'next/image';
+import Header from '../components/Header';
 
 export default function Home() {
   return (
-    <div className='min-h-screen bg-gradient-to-l from-[#313C4A] to-[#323C46]'>
+    <div className='absolute min-w-full min-h-screen bg-gradient-to-l from-[#2A3440] to-[#323C45]'>
       <Head>
         <title>Plix</title>
         <meta name='Plix' content='next app' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <div className='flex justify-center items-center'>
-        <h1>Plix</h1>
-      </div>
+      <Header />
     </div>
   );
 }
