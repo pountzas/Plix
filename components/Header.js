e36610b851c99342e61957f6254b3d68ed0844a6
pountzas <nikos@pountzas.gr>
2022-02-20 21:57:49 +0200

    import plix logo to header

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,10 @@
 import Image from 'next/image';
 
 function Header() {
+  return (
     <header>
       <h1 className='bg-[#232B35] my-3 mx-2 rounded-md p-3 text-white font-semibold font'>
-        PLIX
+        <Image src='/../public/plix-logo-w.png' alt='' height={25} width={60} />
       </h1>
     </header>
   );
