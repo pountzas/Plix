b185554f2eb7f65c2295043c1b586a6159af3b73
pountzas <nikos@pountzas.gr>
2022-02-20 22:10:28 +0200

    add menu item on header

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,19 @@
 import Image from 'next/image';
+import { VscMenu } from 'react-icons/vsc';
 
 function Header() {
   return (
     <header>
       <h1 className='bg-[#232B35] my-3 mx-2 rounded-md p-3 text-white font-semibold font'>
-        <Image src='/../public/plix-logo-w.png' alt='' height={25} width={60} />
+        <div className='flex justify-start items-center'>
+          <VscMenu className='inline-block mr-8 text-2xl' />
+          <Image
+            src='/../public/plix-logo-w.png'
+            alt='logo'
+            height={25}
+            width={60}
+          />
+        </div>
       </h1>
     </header>
   );
