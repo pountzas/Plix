78333687310fc0a06fda9d629b2752f599eee3fd
pountzas <nikos@pountzas.gr>
2022-02-20 21:55:50 +0200

    add basic header component

---
+++ components/Header.js
@@ -0,0 +1,12 @@
+import Image from 'next/image';
+
+function Header() {
+    <header>
+      <h1 className='bg-[#232B35] my-3 mx-2 rounded-md p-3 text-white font-semibold font'>
+        PLIX
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
