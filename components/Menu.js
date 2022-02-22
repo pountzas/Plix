a6d10d3a9a0de0733f6d178cd8036d8aa38104fb
pountzas <nikos@pountzas.gr>
2022-02-22 22:06:46 +0200

    add home tab on menu

--- components/Menu.js
+++ components/Menu.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import { CgHome } from 'react-icons/cg';
 
 function Menu() {
-  return <div>Menu</div>;
+  return (
+    <section className='bg-[#232B35] my-3 mx-2 rounded-md p-3 max-w-xs'>
+      <div className='flex items-center space-x-4 text-gray-300 text-xl'>
+        <CgHome className='text-2xl' />
+        <p>Home</p>
+      </div>
+    </section>
+  );
 }
 
 export default Menu;
