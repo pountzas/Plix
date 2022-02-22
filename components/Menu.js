8466014b5a4f24420f640a50712ae759a1317449
pountzas <nikos@pountzas.gr>
2022-02-22 22:40:49 +0200

    add movies tab on menu

--- components/Menu.js
+++ components/Menu.js
@@ -1,11 +1,16 @@
 import { CgHome } from 'react-icons/cg';
+import { BsFilm } from 'react-icons/bs';
 
 function Menu() {
   return (
-    <section className='bg-[#232B35] my-3 mx-2 rounded-md p-3 max-w-xs'>
+    <section className='bg-[#232B35] space-y-3 my-3 mx-2 rounded-md p-3 max-w-xs'>
       <div className='flex items-center space-x-4 text-gray-300 text-xl'>
         <CgHome className='text-2xl' />
         <p>Home</p>
+      </div>
+      <div className='flex items-center space-x-4 pl-[2px] text-gray-300 text-xl'>
+        <BsFilm />
+        <p>Movies</p>
       </div>
     </section>
   );
