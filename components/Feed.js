318aefae0adcfcf85a1d4cfca42b276e8c31aba2
pountzas <nikos@pountzas.gr>
2022-02-24 22:26:37 +0200

    add monitor icon

--- components/Feed.js
+++ components/Feed.js
@@ -1,12 +1,14 @@
+import { MdViewComfy } from 'react-icons/md';
+
 function Feed() {
   return (
-    <section className='border p-3 mb-3 rounded-md'>
+    <section className='border p-3 mb-3 rounded-md text-gray-300'>
       <div>
         <div className='flex justify-between items-center'>
           <h2>Home</h2>
           <div className='flex items-center space-x-2 object-contain'>
             <p>bar</p>
-            <p>icon</p>
+            <MdViewComfy className='text-3xl' />
           </div>
         </div>
       </div>
