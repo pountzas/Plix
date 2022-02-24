29c06b1d19473d055981ec97297a9316b1db9549
pountzas <nikos@pountzas.gr>
2022-02-24 22:16:57 +0200

    add basic styles on Feed

--- components/Feed.js
+++ components/Feed.js
@@ -1,7 +1,15 @@
 function Feed() {
   return (
-    <section>
-      <h2>Home</h2>
+    <section className='border p-3 mb-3 rounded-md'>
+      <div>
+        <div className='flex justify-between items-center'>
+          <h2>Home</h2>
+          <div className='flex items-center space-x-2 object-contain'>
+            <p>bar</p>
+            <p>icon</p>
+          </div>
+        </div>
+      </div>
       <h3>Latest Movies</h3>
       <h3>Latest TV Shows</h3>
     </section>
