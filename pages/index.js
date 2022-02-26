02b4f1c3a0322cc0eeb9ebe97e997d38b0be41b0
pountzas <nikos@pountzas.gr>
2022-02-26 20:31:42 +0200

    change direction of bg gradient to diagonal

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Dashboard from '../components/Dashboard';
 
 export default function Home() {
   return (
-    <div className='absolute min-w-full min-h-screen bg-gradient-to-l from-[#2A3440] to-[#323C45]'>
+    <div className='absolute min-w-full min-h-screen bg-gradient-to-bl from-[#2A3440] to-[#323C45]'>
       <Head>
         <title>Plix</title>
         <meta name='Plix' content='next app' />
