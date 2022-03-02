8d37b842d6490a335acf34901429a467700fe8c4
pountzas <nikos@pountzas.gr>
2022-03-02 21:06:07 +0200

    add firebase config file

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyA-qJIDEur1UJNMd0qlevyS92v5fnbnvno',
+  authDomain: 'plix-aaef3.firebaseapp.com',
+  projectId: 'plix-aaef3',
+  storageBucket: 'plix-aaef3.appspot.com',
+  messagingSenderId: '568145031745',
+  appId: '1:568145031745:web:18600fe9328523da040cc0',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
