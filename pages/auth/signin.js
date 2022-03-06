ebfa2570daccfff2e56234529cef772fbc5b9f3e
pountzas <nikos@pountzas.gr>
2022-03-06 20:36:53 +0200

    add image logo on signin poage

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,12 +1,19 @@
 import { getProviders, signIn } from 'next-auth/react';
 import Header from '../../components/Header';
+import Image from 'next/image';
 
 export default function SignIn({ providers }) {
   return (
     <section className='bg-gray-800'>
       <Header />
       <div className='flex flex-col items-center justify-center min-h-screen py-2 -mt56 px-14 text-center'>
-        <p className='font-xs italic text-gray-200'>
+        <Image
+          src='/../public/plix-logo-w.png'
+          alt='logo'
+          height={200}
+          width={600}
+        />
+        <p className='font-xs italic text-gray-200 pt-20'>
           This is not a real App, it is built for educational purposes only.
         </p>
         {Object.values(providers).map((provider) => (
