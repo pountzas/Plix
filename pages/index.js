11fb296e2d6e7f8586dc17adba2ace587b35079e
pountzas <nikos@pountzas.gr>
2022-03-07 23:07:25 +0200

    use router to pages if session

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,22 @@
 import Head from 'next/head';
 import Header from '../components/Header';
 import Dashboard from '../components/Dashboard';
+import { useSession } from 'next-auth/react';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 export default function Home() {
+  const { data: session } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!session) {
+      router.push('/auth/signin');
+    } else {
+      router.push('/');
+    }
+  }, [router, session]);
+
   return (
     <div className='absolute min-w-full min-h-screen bg-gradient-to-bl from-[#2A3440] to-[#323C45]'>
       <Head>
@@ -11,7 +25,7 @@ export default function Home() {
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <Header />
-      <Dashboard />
+      {session && <Dashboard />}
     </div>
   );
 }
