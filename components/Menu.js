2fa75beaa16fe4d2c0ddaa2af87c82ebef5cc1f6
pountzas <nikos@pountzas.gr>
2022-03-08 14:07:47 +0200

    add media folder icon

--- components/Menu.js
+++ components/Menu.js
@@ -1,14 +1,16 @@
 import { CgHome } from 'react-icons/cg';
 import { BsFilm } from 'react-icons/bs';
 import { MdMonitor } from 'react-icons/md';
+import { AiFillFolderAdd } from 'react-icons/ai';
 
 function Menu() {
   return (
-    <section className='bg-[#232B35] space-y-3 mb-3 mx-3 rounded-md p-3 pr-32 min-h-[90vh]'>
+    <section className='bg-[#232B35] space-y-3 mb-3 mx-3 rounded-md p-3 pr-48 min-h-[90vh]'>
       <div className='flex items-center space-x-4 text-gray-300 text-xl'>
         <CgHome className='text-2xl' />
         <p>Home</p>
       </div>
+
       <div className='flex items-center space-x-4 pl-[2px] text-gray-300 text-xl'>
         <BsFilm />
         <p>Movies</p>
@@ -16,6 +18,11 @@ function Menu() {
       <div className='flex items-center space-x-4 text-gray-300 text-xl'>
         <MdMonitor className='text-2xl' />
         <p>TV Shows</p>
+      </div>
+
+      <div className='flex items-center space-x-4 text-gray-300 text-xl'>
+        <AiFillFolderAdd />
+        <p>Add Media</p>
       </div>
     </section>
   );
