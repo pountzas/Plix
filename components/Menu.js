9f68a81c32ea32bc35c7b4eae52b9f2100922f6a
pountzas <nikos@pountzas.gr>
2022-03-09 22:26:54 +0200

    setOpen add media button onClick

--- components/Menu.js
+++ components/Menu.js
@@ -9,21 +9,24 @@ function Menu() {
   const [open, setOpen] = useRecoilState(modalState);
   return (
     <section className='bg-[#232B35] space-y-3 mb-3 mx-3 rounded-md p-3 pr-48 min-h-[90vh]'>
-      <div className='flex items-center space-x-4 text-gray-300 text-xl'>
+      <div className='flex flex-nowrap cursor-pointer items-center space-x-4 text-gray-300 text-xl'>
         <CgHome className='text-2xl' />
         <p>Home</p>
       </div>
 
-      <div className='flex items-center space-x-4 pl-[2px] text-gray-300 text-xl'>
+      <div className='flex flex-nowrap cursor-pointer items-center space-x-4 pl-[2px] text-gray-300 text-xl'>
         <BsFilm />
         <p>Movies</p>
       </div>
-      <div className='flex items-center space-x-4 text-gray-300 text-xl'>
+      <div className='flex flex-nowrap cursor-pointer items-center space-x-4 text-gray-300 text-xl'>
         <MdMonitor className='text-2xl' />
         <p>TV Shows</p>
       </div>
 
-      <div className='flex items-center space-x-4 text-gray-300 text-xl'>
+      <div
+        onClick={() => setOpen(true)}
+        className='flex flex-nowrap cursor-pointer items-center space-x-4 text-gray-300 text-xl'
+      >
         <AiFillFolderAdd />
         <p>Add Media</p>
       </div>
