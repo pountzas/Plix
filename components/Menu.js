d1e605b1054293fba0864f4387a1cb4e9c107f2e
pountzas <nikos@pountzas.gr>
2022-03-09 22:01:49 +0200

    add modalState to Menu lvl

--- components/Menu.js
+++ components/Menu.js
@@ -2,8 +2,11 @@ import { CgHome } from 'react-icons/cg';
 import { BsFilm } from 'react-icons/bs';
 import { MdMonitor } from 'react-icons/md';
 import { AiFillFolderAdd } from 'react-icons/ai';
+import { useRecoilState } from 'recoil';
+import { modalState } from '../atoms/modalAtom';
 
 function Menu() {
+  const [open, setOpen] = useRecoilState(modalState);
   return (
     <section className='bg-[#232B35] space-y-3 mb-3 mx-3 rounded-md p-3 pr-48 min-h-[90vh]'>
       <div className='flex items-center space-x-4 text-gray-300 text-xl'>
