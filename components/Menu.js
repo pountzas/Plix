4e9283d44e4b06ee9bd4d3225ac62bf6204842b8
pountzas <nikos@pountzas.gr>
2022-03-11 16:48:47 +0200

    add menuSizeState on Menu to disable text

--- components/Menu.js
+++ components/Menu.js
@@ -3,24 +3,25 @@ import { BsFilm } from 'react-icons/bs';
 import { MdMonitor } from 'react-icons/md';
 import { AiFillFolderAdd } from 'react-icons/ai';
 import { useRecoilState } from 'recoil';
-import { modalState } from '../atoms/modalAtom';
+import { modalState, menuSizeState } from '../atoms/modalAtom';
 
 function Menu() {
   const [open, setOpen] = useRecoilState(modalState);
+  const [menuSize, setMenuSize] = useRecoilState(menuSizeState);
   return (
     <section className='bg-[#232B35] space-y-3 mb-3 mx-3 rounded-md p-3 pr-48 min-h-[90vh]'>
       <div className='flex flex-nowrap cursor-pointer items-center space-x-4 text-gray-300 text-xl'>
         <CgHome className='text-2xl' />
-        <p>Home</p>
+        {!menuSize && <p>Home</p>}
       </div>
 
       <div className='flex flex-nowrap cursor-pointer items-center space-x-4 pl-[2px] text-gray-300 text-xl'>
         <BsFilm />
-        <p>Movies</p>
+        {!menuSize && <p>Movies</p>}
       </div>
       <div className='flex flex-nowrap cursor-pointer items-center space-x-4 text-gray-300 text-xl'>
         <MdMonitor className='text-2xl' />
-        <p>TV Shows</p>
+        {!menuSize && <p>TV Shows</p>}
       </div>
 
       <div
@@ -28,7 +29,7 @@ function Menu() {
         className='flex flex-nowrap cursor-pointer items-center space-x-4 text-gray-300 text-xl'
       >
         <AiFillFolderAdd />
-        <p>Add Media</p>
+        {!menuSize && <p>Add Media</p>}
       </div>
     </section>
   );
