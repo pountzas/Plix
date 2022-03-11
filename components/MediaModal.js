4c1f6eef0195f6d31b1d051dbd58e6a9697840fd
pountzas <nikos@pountzas.gr>
2022-03-11 21:37:25 +0200

    add mediaModal basic structure & styles w/ close modal state

--- components/MediaModal.js
+++ components/MediaModal.js
@@ -4,7 +4,25 @@ import { modalState } from '../atoms/modalAtom';
 function MediaModal() {
   const [open, setOpen] = useRecoilState(modalState);
 
-  return <div className='bg-black text-3xl text-white'>MediaModal</div>;
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <>
+      {open && (
+        <div className='fixed z-10 inset-0 overflow-y-auto bg-black text-3xl text-white'>
+          <div className='flex items-center justify-between min-h-[800px] sm:min-h-screen pt-4 px-4 pb-20 text-center '>
+            <h2>Add Media</h2>
+            <button className='bg-gray-600' onClick={handleClose}>
+              close
+            </button>
+          </div>
+          MediaModal
+        </div>
+      )}
+    </>
+  );
 }
 
 export default MediaModal;
