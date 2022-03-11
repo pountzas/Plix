87a6c7612b2c778490d3d178d59076ef1f81203d
pountzas <nikos@pountzas.gr>
2022-03-11 22:30:59 +0200

    add menuSize state to media Feeds length

--- components/Feed.js
+++ components/Feed.js
@@ -1,7 +1,11 @@
 import { MdViewComfy } from 'react-icons/md';
 import MediaCard from './MediaCard';
+import { useRecoilState } from 'recoil';
+import { modalState, menuSizeState } from '../atoms/modalAtom';
 
 function Feed() {
+  const [menuSize, setMenuSize] = useRecoilState(menuSizeState);
+
   return (
     <section className='p-3 mb-3 mr-3 rounded-md text-2xl text-gray-300'>
       <div className='flex justify-between items-center'>
@@ -11,7 +15,13 @@ function Feed() {
           <MdViewComfy className='text-3xl' />
         </div>
       </div>
-      <div className='md:max-w-[50vw] lg:max-w-[65vw] xl:max-w-[72vw] 2xl:max-w-[81vw]'>
+      <div
+        className={`${
+          menuSize
+            ? 'md:max-w-[75vw] lg:max-w-[86vw] xl:max-w-[89vw] 2xl:max-w-[94vw]'
+            : 'md:max-w-[50vw] lg:max-w-[65vw] xl:max-w-[72vw] 2xl:max-w-[81vw]'
+        } `}
+      >
         <div>
           <h3>Latest Movies</h3>
           <div className='pl-3 flex space-x-7 overflow-x-scroll scrollbar-hide'>
