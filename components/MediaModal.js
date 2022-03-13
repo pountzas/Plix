da5d1e12e1ed3f49cfd93f9a1d97e7dc6a6b6412
pountzas <nikos@pountzas.gr>
2022-03-13 18:59:00 +0200

    add modal header

--- components/MediaModal.js
+++ components/MediaModal.js
@@ -12,7 +12,9 @@ function MediaModal() {
   return (
     <>
       {open && (
+        <div className='fixed z-10 inset-52 overflow-y-auto bg-black text-white border-gray-900 border-4 max-h-[80vh] max-w-[80vw] rounded-xl'>
           {/* modal header  */}
+          <div className='flex items-center bg-gray-800  justify-between  pt-4 px-4 text-gray-400 pb-3'>
             <h2>Add Media</h2>
             <button className='' onClick={handleClose}>
               <AiOutlineCloseCircle className='text-2xl' />
