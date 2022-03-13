3b6dbb6ca9abbacad5ff6ade9e4ddc62fa7be457
pountzas <nikos@pountzas.gr>
2022-03-13 19:14:52 +0200

    add plus icon to modal header

--- components/MediaModal.js
+++ components/MediaModal.js
@@ -1,6 +1,6 @@
 import { useRecoilState } from 'recoil';
 import { modalState } from '../atoms/modalAtom';
-import { AiOutlineCloseCircle } from 'react-icons/ai';
+import { AiOutlineCloseCircle, AiOutlinePlus } from 'react-icons/ai';
 
 function MediaModal() {
   const [open, setOpen] = useRecoilState(modalState);
@@ -14,8 +14,11 @@ function MediaModal() {
       {open && (
         <div className='fixed z-10 inset-52 overflow-y-auto bg-black text-white border-gray-900 border-4 max-h-[80vh] max-w-[80vw] rounded-xl'>
           {/* modal header  */}
-          <div className='flex items-center bg-gray-800  justify-between  pt-4 px-4 text-gray-400 pb-3'>
-            <h2>Add Media</h2>
+          <div className='flex items-center bg-gray-800 justify-between pt-4 px-4 text-gray-400 pb-3'>
+            <div className='flex space-x-2 items-center'>
+              <AiOutlinePlus />
+              <h2>Add Library</h2>
+            </div>
             <button className='' onClick={handleClose}>
               <AiOutlineCloseCircle className='text-2xl' />
             </button>
