217b917104a235258ca63f085e97dacd72f60471
pountzas <nikos@pountzas.gr>
2022-03-16 21:12:28 +0200

    adjust general styles on mediaModal

--- components/MediaModal.js
+++ components/MediaModal.js
@@ -12,7 +12,7 @@ function MediaModal() {
   return (
     <>
       {open && (
-        <div className='fixed z-10 inset-52 overflow-y-auto bg-black text-white border-gray-900 border-4 max-h-[80vh] max-w-[80vw] rounded-xl'>
+        <div className='flex flex-col justify-between fixed z-10 inset-52 overflow-y-auto bg-gray-900 text-white border-gray-900 border-4 max-w-[80vw] rounded-xl'>
           {/* modal header  */}
           <div className='flex items-center bg-gray-800 justify-between pt-4 px-4 text-gray-400 pb-3'>
             <div className='flex space-x-2 items-center'>
