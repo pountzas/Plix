52cbf77114e48f5a1ded59d95534f64de2660312
nikos pountzas <nikos.pountzas@lightsourcelabs.com>
2022-04-08 14:06:53 +0300

    run getMediaData

--- components/MediaModal.js
+++ components/MediaModal.js
@@ -78,6 +78,7 @@ function MediaModal() {
               : console.log(name + ' not found ' + files[i].webkitRelativePath);
           }
         };
+        getMediaData();
       }
     }
     console.log(MovieFiles);
