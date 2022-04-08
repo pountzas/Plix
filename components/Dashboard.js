a0e3500f39612dce83e09e7d9d3ff2f66eab7fa9
nikos pountzas <nikos.pountzas@lightsourcelabs.com>
2022-04-08 17:50:18 +0300

    Dash: imp MediaItem, recoil & mediaItemState to use between Feed & MediaItem

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -1,11 +1,16 @@
 import Feed from './Feed';
 import Menu from './Menu';
+import MediaItem from './MediaItem';
+import { useRecoilState } from 'recoil';
+import { mediaItemState } from '../atoms/modalAtom';
 
 function Dashboard() {
+  const [mediaItem, setMediaItem] = useRecoilState(mediaItemState);
   return (
     <div className='flex'>
       <Menu />
-      <Feed />
+      {!mediaItem ? <Feed /> : <MediaItem />}
+      {/* <Feed /> */}
     </div>
   );
 }
