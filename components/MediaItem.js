903ddc693e008da9f780bee1e5eb339694ba98c8
pountzas <nikos@pountzas.gr>
2022-04-10 00:14:41 +0300

    MediaItem: add ReactPlayer w/ settings passed from MediaItemProps

--- components/MediaItem.js
+++ components/MediaItem.js
@@ -3,6 +3,7 @@ import { mediaItemState } from '../atoms/modalAtom';
 import MediaItemProps from './props/MediaItemProps';
 import { AiOutlineCloseCircle } from 'react-icons/ai';
 import { FaBackward } from 'react-icons/fa';
+import ReactPlayer from 'react-player';
 
 function MediaItem() {
   const [mediaItem, setMediaItem] = useRecoilState(mediaItemState);
@@ -15,7 +16,24 @@ function MediaItem() {
       <button className='pt-3' onClick={handleClose}>
         <FaBackward className='text-gray-500 text-2xl' />
       </button>
-      <div></div>
+        <ReactPlayer
+          // className='rounded'
+          config={{
+            file: {
+              attributes: {
+                controlsList: 'nodownload',
+                volume: 1,
+                controls: true,
+                color: '#fff',
+              },
+            },
+          }}
+          url={MediaItemProps.objurl}
+          controls
+          pip={true}
+          width='640px'
+          height='400px'
+        />
     </div>
   );
 }
