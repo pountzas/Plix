3df35bfbb183a4b8d31d558ae5ce1484e3c29659
pountzas <nikos@pountzas.gr>
2022-04-14 15:03:18 +0300

    create atom homeMovieState

--- atoms/modalAtom.js
+++ atoms/modalAtom.js
@@ -15,6 +15,11 @@ export const mediaItemState = atom({
   default: false,
 });
 
+export const homeMovieState = atom({
+  key: 'homeMovieState',
+  default: false,
+});
+
 export const mediaValueState = atom({
   key: 'mediaValueState',
   default: {},
