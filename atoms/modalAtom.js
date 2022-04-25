171c407ee4434d7320e4030ae41b26f2088ab88f
pountzas <nikos@pountzas.gr>
2022-04-25 23:53:12 +0300

    add atom bgImageUrl recoil state

--- atoms/modalAtom.js
+++ atoms/modalAtom.js
@@ -50,6 +50,11 @@ export const tvMenuState = atom({
   default: false,
 });
 
+export const bgImageUrl = atom({
+  key: 'bgImageUrl',
+  default: '',
+});
+
 export const mediaValueState = atom({
   key: 'mediaValueState',
   default: {},
