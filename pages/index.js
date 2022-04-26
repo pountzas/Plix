0f984bd78dd275d2fe2d1e60afeb0225abe6f6c6
pountzas <nikos@pountzas.gr>
2022-04-26 12:24:48 +0300

    Home: change syles accoding to movieMenu

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,14 @@ import MediaModal from '../components/MediaModal';
 import { useSession } from 'next-auth/react';
 import { useEffect } from 'react';
 import { useRecoilState } from 'recoil';
-import { bgImageUrl, imageState } from '../atoms/modalAtom';
+import { bgImageUrl, imageState, movieMenuState } from '../atoms/modalAtom';
 import Image from 'next/image';
 
 export default function Home() {
   const { data: session } = useSession();
   const [bgImage, setBgImage] = useRecoilState(bgImageUrl);
   const [image, setImage] = useRecoilState(imageState);
+  const [movieMenu, setMovieMenu] = useRecoilState(movieMenuState);
 
   useEffect(() => {
     if (image) {
@@ -21,12 +22,17 @@ export default function Home() {
 
   return (
     <div
-      className={`relative min-w-full min-h-screen bg-gradient-to-bl from-[#2A3440] to-[#323C45]`}
+      className={
+        movieMenu
+          ? `min-h-[8000px] `
+          : `min-h-screen ` +
+            `relative min-w-full bg-gradient-to-bl from-[#2A3440] to-[#323C45]`
+      }
     >
       {image && (
         <div>
           <Image
-            className='z-0 opacity-10 min-w-full min-h-screen '
+            className='z-0 opacity-20 min-w-full min-h-screen '
             src={`https://www.themoviedb.org/t/p/original${bgImage}`}
             alt=''
             layout='fill'
