81fbce9bd3eb853cff1944a64774c97d2dc9fa4f
pountzas <nikos@pountzas.gr>
2022-06-01 12:57:25 +0300

    create Slider bg Props file

---
+++ components/props/SliderBgOpacity.js
@@ -0,0 +1,37 @@
+const SliderBgOpacity = {
+  0: {
+    opacityValue: 'hidden'
+  },
+  1: {
+    opacityValue: 'opacity-[0.1]'
+  },
+  2: {
+    opacityValue: 'opacity-[0.2]'
+  },
+  3: {
+    opacityValue: 'opacity-[0.3]'
+  },
+  4: {
+    opacityValue: 'opacity-[0.4]'
+  },
+  5: {
+    opacityValue: 'opacity-[0.5]'
+  },
+  6: {
+    opacityValue: 'opacity-[0.6]'
+  },
+  7: {
+    opacityValue: 'opacity-[0.7]'
+  },
+  8: {
+    opacityValue: 'opacity-[0.8]'
+  },
+  9: {
+    opacityValue: 'opacity-[0.9]'
+  },
+  10: {
+    opacityValue: 'opacity-[1]'
+  }
+};
+
+export default SliderBgOpacity;
