d0cf945eed5b80b64084247ac58cf44127dfa85c
pountzas <nikos@pountzas.gr>
2022-06-01 14:47:27 +0300

    refactor SliderBgOpacity

--- components/props/SliderBgOpacity.js
+++ components/props/SliderBgOpacity.js
@@ -1,37 +1,15 @@
 const SliderBgOpacity = {
-  0: {
-    opacityValue: 'hidden'
-  },
-  1: {
-    opacityValue: 'opacity-[0.1]'
-  },
-  2: {
-    opacityValue: 'opacity-[0.2]'
-  },
-  3: {
-    opacityValue: 'opacity-[0.3]'
-  },
-  4: {
-    opacityValue: 'opacity-[0.4]'
-  },
-  5: {
-    opacityValue: 'opacity-[0.5]'
-  },
-  6: {
-    opacityValue: 'opacity-[0.6]'
-  },
-  7: {
-    opacityValue: 'opacity-[0.7]'
-  },
-  8: {
-    opacityValue: 'opacity-[0.8]'
-  },
-  9: {
-    opacityValue: 'opacity-[0.9]'
-  },
-  10: {
-    opacityValue: 'opacity-[1]'
-  }
+  0: 'hidden',
+  1: 'opacity-[0.1]',
+  2: 'opacity-[0.2]',
+  3: 'opacity-[0.3]',
+  4: 'opacity-[0.4]',
+  5: 'opacity-[0.5]',
+  6: 'opacity-[0.6]',
+  7: 'opacity-[0.7]',
+  8: 'opacity-[0.8]',
+  9: 'opacity-[0.9]',
+  10: 'opacity-[1]'
 };
 
 export default SliderBgOpacity;
